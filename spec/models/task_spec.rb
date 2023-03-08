4538c8e88df859709e27d197b745a1473e3c3baa
MoussaGeek <moussadiakite1963@gmail.com>
2023-03-08 00:44:36 +0000

    MAJ

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  describe "validation" do
+  describe "Validation" do
     it "La validation de la tâche est désactivée si le titre n'est pas saisi" do
       task = Task.new(title: nil, description: "test", status: :todo, deadline: Time.current)
       expect(task).to be_invalid
@@ -28,7 +28,7 @@ RSpec.describe Task, type: :model do
 
     it "La validation de la tâche est valide si la date d'échéance est la date du jour" do
       task = Task.new(title: "test", description: "test", status: :todo, deadline: Date.today)
-      expect(task).to be_invalid
+      expect(task).to be_valid
     end
   end
 end
